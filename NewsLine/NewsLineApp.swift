43403ff50f1ed34735d5ba1a7cbf730d38f890d3
LeChris <bk2mua@gmail.com>
2023-09-01 15:32:30 +0300

    Initial Commit

---
+++ NewsLine/NewsLineApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsLineApp.swift
+//  NewsLine
+//
+//  Created by Le Chris on 01.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsLineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
